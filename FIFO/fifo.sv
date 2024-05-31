66082b52584138814cfe67132826270bbd472e71
Sharma M, Karthikeya <km304@gatech.edu>
2024-05-31 17:52:56 -0500

    updating all codes

--- FIFO/fifo.sv
+++ FIFO/fifo.sv
@@ -2,14 +2,16 @@
 
 module fifo (input clk, rst, wr, rd, input [7:0] din, output reg [7:0] dout, output full, empty);
 
-    //Read and write pointers
+  //Read and write pointers
   reg [3:0] rptr = 0;
   reg [3:0] wptr = 0;
+
+  //common pointer cnt for finding the effective position of the pointer in FIFO - for read and write.
   reg [4:0] cnt = 0;
 
     //Initializing memory
     reg [7:0] mem [15:0];
-    
+
     always@(posedge clk) begin
         if (rst) begin
             wptr <= 0;
@@ -17,12 +19,12 @@ module fifo (input clk, rst, wr, rd, input [7:0] din, output reg [7:0] dout, out
             cnt <= 0;
         end
         else if (rd && !empty) begin
-          	dout <= mem[rptr];
+            dout <= mem[rptr];
             rptr <= rptr + 1;
             cnt <= cnt - 1;
         end
         else if (wr && !full) begin
-          	mem[wptr] <= din;
+            mem[wptr] <= din;
             wptr <= wptr + 1;
             cnt <= cnt + 1;
         end
